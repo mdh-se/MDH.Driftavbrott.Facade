401436d1b4a7a22d3abe51a251c620881eb9cd8c
Edward McNamara <edward.mcnamara@mdu.se>
2024-03-15 13:57:51 +0100

    DOTNET-197 DOTNET-268 Nytt versionsnummer för framtida major release 3.0.0 (Ej redo för release!)

--- MDH.Driftavbrott.Facade/Properties/AssemblyInfo.cs
+++ MDH.Driftavbrott.Facade/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MDH.Driftavbrott.Facade")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Mälardalens högskola")]
+[assembly: AssemblyCompany("Mälardalens universitet")]
 [assembly: AssemblyProduct("MDH.Driftavbrott.Facade")]
-[assembly: AssemblyCopyright("Copyright © Mälardalens högskola 2017")]
+[assembly: AssemblyCopyright("Copyright © Mälardalens universitet 2017–2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.8.2.*")]
-[assembly: AssemblyFileVersion("2.8.2")]
+[assembly: AssemblyVersion("3.0.0.*")]
+[assembly: AssemblyFileVersion("3.0.0")]
