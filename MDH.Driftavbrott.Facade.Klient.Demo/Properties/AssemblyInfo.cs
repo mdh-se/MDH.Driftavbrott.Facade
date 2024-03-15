60cd97729056283dcb51716c7f9c60c366d7074b
Edward McNamara <edward.mcnamara@mdu.se>
2024-03-15 15:36:29 +0100

    DOTNET-197 DOTNET-268 Justera namn på två underprojekt till det nya begreppet/suffixet .demo (istället för varianter av -test som kunde upplevas förvirrande då de varken innehåller enhetstester eller integrationstester).

--- MDH.Driftavbrott.Facade.Klient.Demo/Properties/AssemblyInfo.cs
+++ MDH.Driftavbrott.Facade.Klient.Demo/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MDH.Driftavbrott.Facade.MonitorTest")]
+[assembly: AssemblyTitle("MDH.Driftavbrott.Facade.Klient.Demo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MDH.Driftavbrott.Facade.MonitorTest")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("MDH.Driftavbrott.Facade.Klient.Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4991c8cf-65d0-47b6-9ffe-2dc158a8c66a")]
+[assembly: Guid("88E4CE26-55CA-4FB4-B52C-9127F2991371")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- MDH.Driftavbrott.Facade.Monitor.Demo/Properties/AssemblyInfo.cs
+++ MDH.Driftavbrott.Facade.Monitor.Demo/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MDH.Driftavbrott.Facade.KlientTest")]
+[assembly: AssemblyTitle("MDH.Driftavbrott.Facade.Monitor.Demo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MDH.Driftavbrott.Facade.KlientTest")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("MDH.Driftavbrott.Facade.Monitor.Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("88E4CE26-55CA-4FB4-B52C-9127F2991371")]
+[assembly: Guid("4991c8cf-65d0-47b6-9ffe-2dc158a8c66a")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
